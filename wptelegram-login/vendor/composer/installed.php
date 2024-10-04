bce21f2701cb9aaeee323f885a2cf77c41da7069
Sean Murphy <sean@iamseanmurphy.com>
2024-10-04 17:49:52 +0000

    1.10.24

---
+++ wptelegram-login/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpsocio/wptelegram-login',
+        'pretty_version' => '1.10.24',
+        'version' => '1.10.24.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpsocio/wp-utils' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f7768dcc7d44830a0c58b7f6e0696bdc846781b8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpsocio/wp-utils',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpsocio/wptelegram-login' => array(
+            'pretty_version' => '1.10.24',
+            'version' => '1.10.24.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
